4f0f62ee74e116955c069a0a82d19225f3762b98
usmanabid-rydelinx <usman@rydelinx.com>
2022-01-20 10:25:16 +0500

    Update ConfigJS

--- config.js
+++ config.js
@@ -16,21 +16,3 @@ module.exports = {
     url: HOST_URL,
     databaseUrl: DATABASE_URL
 }
-
-
-/* const admin = require('firebase-admin')
-const database = require('firebase-admin/firestore')
-
-const serviceAccount = require('./serviceAccountKey.json')
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://aps-web-4c55e-default-rtdb.firebaseio.com"
-})
-
-const db = database.getFirestore()
-const rdb = admin.database()
-const auth = admin.auth()
-const notify = admin.messaging()
-
-module.exports = {db, rdb, auth, notify}*/
