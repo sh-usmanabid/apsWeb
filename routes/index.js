b54cd6265a68e99b6cf6f66180801f329e535b04
usmanabid-rydelinx <usman@rydelinx.com>
2022-01-22 19:51:39 +0500

    adding swagger

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,54 @@ const { setFood, setMedicine, setLocation, setRadius } = require('../controllers
 
 const router = express.Router()
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      User:
+ *          type: Object
+ *          required:
+ *              - displayName
+ *              - email
+ *              - password
+ *          properties:
+ *              displayName:
+ *                  type: string
+ *                  description: Display Name
+ *              email:
+ *                  type: email
+ *                  description: Email Address
+ *              password:
+ *                  type: password
+ *                  description: Password
+ *          example:
+ *              displayName: Jon Doe
+ *              email: user@email.com
+ *              password: password
+ */
+
+/**
+ * @swagger
+ * /api/create-user:
+ *  post:
+ *      summary: Creates a new user.
+ *      tags: [User]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/User'
+ *      responses:
+ *          '200':
+ *              description: User Created
+ *              contents:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/User'
+ */
 router.post('/create-user', createUser)
+
 router.post('/set-food', setFood)
 router.post('/set-medicine', setMedicine)
 router.post('/set-location', setLocation)
