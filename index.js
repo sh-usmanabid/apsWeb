a8783d08d71c48b4bedd26e6e86b68d3ba5f6429
usmanabid-rydelinx <usman@rydelinx.com>
2022-01-27 01:12:54 +0500

    new

--- index.js
+++ index.js
@@ -45,14 +45,14 @@ app.use('/logs', express.static('logs'))
 app.set("view engine", "pug")
 app.set("views", path.join(__dirname, "views"))
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
     fs.readFile('./logs/info.txt', 'utf8' , (error, data) => {
         const logs = data.trim().split('\n')
         res.render('index', { logs: logs })
     })
-})
+}) */
 
-app.get('/food', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile('./views/food.html', { root: __dirname })
 })
 
