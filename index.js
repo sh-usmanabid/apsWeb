edaff8b7e0864c522143136849ea25b077142a82
usmanabid-rydelinx <usman@rydelinx.com>
2022-01-27 18:15:27 +0500

    adding cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const path = require("path")
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const swaggerUI = require('swagger-ui-express')
 const swaggerJsDoc = require('swagger-jsdoc')
@@ -35,6 +36,7 @@ const app = express()
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs))
 
+app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json())
 
